3cc552f25c253ecfa8be86ff195fb33f25a234b9
dagedaya <2>
2020-11-06 17:57:36 +0800

    echostr

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -25,7 +25,7 @@ class WxController extends Controller
             $xml_str = file_get_contents('php://input');
             //记录日志
             file_put_contents('wx_event.log',$xml_str,'FILE_APPEND');
-            echo "$echostr";
+//            echo "$echostr";
 //            die;
             //2.把xml文本转换成php的数组或者对象
             $data = simplexml_load_string($xml_str, 'SimpleXMLElement', LIBXML_NOCDATA);
