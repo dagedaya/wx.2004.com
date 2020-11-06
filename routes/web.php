72a50da3b50f0bbd2cb820885301ca668ff10280
dagedaya <2>
2020-11-06 15:40:50 +0800

    缓存

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/info',function (){
     phpinfo();
 });
 //微信开发者服务器接入
-Route::get('/wx','WxController@checkSignature');
+Route::post('/wx','WxController@checkSignature');
 //获取access_token
 Route::get('/access_token','WxController@access_token');
 
