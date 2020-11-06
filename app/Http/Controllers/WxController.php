df593625844233425d13497cdf6a0da684c380ff
dagedaya <2>
2020-11-06 16:47:28 +0800

    追加

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -24,7 +24,7 @@ class WxController extends Controller
             //1.接收数据
             $xml_str=file_get_contents('php://input');
             //记录日志
-            file_put_contents('wx_event.log',$xml_str);
+            file_put_contents('wx_event.log',$xml_str,'FILE_APPEND');
             echo "$echostr";
             die;
 //            //2.把xml文本转换成php的数组或者对象
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::get('/info',function (){
     phpinfo();
 });
-//微信开发者服务器接入
+//微信开发者服务器接入(即支持get又支持post)
 Route::match(['get','post'],'/wx','WxController@checkSignature');
 //获取access_token
 Route::get('/access_token','WxController@access_token');
