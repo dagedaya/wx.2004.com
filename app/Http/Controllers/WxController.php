657734a96afb0e4ae574ac7cf1610f131a18433b
dagedaya <2>
2020-11-07 17:44:57 +0800

    3

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -54,7 +54,7 @@ class WxController extends Controller
                     file_put_contents('user_access.log',$url);
                     $user=file_get_contents($url);
                     $users=json_decode($user,true);
-                    file_put_contents('user_access.log1',$users.'\r\n',FILE_APPEND);
+//                    file_put_contents('user_access.log1',$users.'\r\n',FILE_APPEND);
 //                    die;
                     $data=[
                         'subscribe'=>$users['subscribe'],
