74f293a3ceb89ed658fb91d0395f47de659ccd8a
dagedaya <2>
2020-11-07 18:20:55 +0800

    6

--- app/Model/WxUserModel.php
+++ app/Model/WxUserModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class WxUserModel extends Model
 {
     //设置表名
-    protected $table="WxUserModel";
+    protected $table="wxuser";
     //设置主键
     protected $primaryKey="id";
     //设置时间戳
